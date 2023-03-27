ad5a20aac9aa8d6dd213af237c6e94d750017003
Kimmyeonghwan <myeonghwan2@kookmin.ac.kr>
2023-03-27 13:04:38 +0900

    Feat: 템플릿을 사용하여 URL로 질문 데이터 조회 및 답변 기능 개발

--- pybo/views.py
+++ pybo/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Question, Answer
 
 def index(request):
     question_list = Question.objects.order_by('-create_date')
@@ -14,3 +15,13 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk = question_id)
     context = {'question' : question}
     return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    # 아래 코드도 작동함
+    """
+    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    answer.save()
+    """
+    return redirect('pybo:detail', question_id=question.id)
