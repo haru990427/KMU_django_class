c88996ddc67fc0c35f4103e69e081fd94d007ea7
Kimmyeonghwan <myeonghwan2@kookmin.ac.kr>
2023-04-17 12:50:47 +0900

    Feat: 글쓴이 추가(모델 및 뷰, html 수정)

--- pybo/views.py
+++ pybo/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponseNotAllowed
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question, Answer
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     page = request.GET.get('page','1') # 페이지
@@ -22,18 +22,20 @@ def detail(request, question_id):
     context = {'question' : question}
     return render(request, 'pybo/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if request.method == "POST":
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit = False)
+            answer.author = request.user  # author 속성에 로그인 계정 저장
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
             return redirect('pybo:detail', question_id = question.id)
     else:
-        return HttpResponseNotAllowed('Only POST is possible.') # 오직 POST 메소드만 사용 가능
+        form = AnswerForm()
     context = {'question': question, 'form': form}
     return render(request, 'pybo/question_detail.html', context)
 
@@ -46,11 +48,13 @@ def answer_create(request, question_id):
     """
     # return redirect('pybo:detail', question_id=question.id)
 
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit = False)
+            question.author = request.user  # author 속성에 로그인 계정 저장
             question.create_date = timezone.now()
             question.save()
             return redirect('pybo:index')
